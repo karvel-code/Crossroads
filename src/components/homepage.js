42622f3055d2772d08a5aed1e1c989226684faac
karvel-code <velgithinji@gmail.com>
2022-05-23 17:00:58 +0300

    Add HomePage code framework

---
+++ src/components/homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Github Commits</h1>
+            </div>
+        )
+    } 
+}
+
+export default HomePage;
