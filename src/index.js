e08f834399776b265d917a12297ba60d0fae2263
karvel-code <velgithinji@gmail.com>
2022-05-23 17:03:56 +0300

    Render homepage in Index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import HomePage from './components/homepage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HomePage/>
   </React.StrictMode>
 );
 
