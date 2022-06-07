2194656fda7fc439da225c27e4b1a62a29030339
Elvis Githinji <velgithinji@gmail.com>
2022-06-07 19:49:33 +0300

    Style main page just like figma design

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,15 @@ import React from "react";
 class Main extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Main section</h1>
-                <p> Main section contents</p>
+            <div className="mainPage">
+                <h1 className="mainTitle">Fun Facts About React</h1>
+                <ul className="listUl">
+                    <li>Was first released in 2013</li>
+                    <li>Was originally created by Elvis Kariithi</li>
+                    <li>Has well over 100 stars on Github</li>
+                    <li>Is maintained by Facebook</li>
+                    <li>Powers thousands of enterprise apps including mobile apps</li>
+                </ul>
             </div>
         )
     } 
