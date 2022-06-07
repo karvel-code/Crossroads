0dab0c3960ff42e865ae1c4904efeb319583d556
Elvis Githinji <velgithinji@gmail.com>
2022-06-07 06:39:44 +0300

    styled navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactLogo from "../images/react-logo.png";
+import "../../src/main.css";
 
 class Header extends React.Component {
     render() {
         return(
-            <div>
-                <img src={ReactLogo} />
-                <h1>React Facts</h1>
-                <h4>React Course - Project 1</h4>
-            </div>
+            <nav className="nav-bar">
+                <div className="nav-contents">
+                    <img src={ReactLogo} className="logoIMage" />
+                    <h1 className="reactFacts">React Facts</h1>
+                </div>
+                
+                <h3 className="reactHead">React Course - Project 1</h3>
+            </nav>
         )
     }
 }
